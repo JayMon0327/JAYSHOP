978ac30ddf2e352d8671ce7e7a54e6c4222e981b
JayMon0327 <kioplm0211@naver.com>
2024-01-02 23:42:44 +0900

    chapter03-JSX

--- jayshop-front/src/index.js
+++ jayshop-front/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from './chapter_03/Library';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Library />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
